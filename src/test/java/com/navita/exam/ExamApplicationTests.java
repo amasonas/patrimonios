2f92f786edb6d1236acb6d5877e07fddbf80a3c8
Amasonas <amasonas,vieira@gmail.com>
2020-04-12 16:44:23 -0300

    Classes referentes ao teste de números irmãos

--- src/test/java/com/navita/exam/ExamApplicationTests.java
+++ src/test/java/com/navita/exam/ExamApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
